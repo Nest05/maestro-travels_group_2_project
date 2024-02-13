a5ed1c8acff7337c7a5daebf1fb9ec7f0b926e18
OumaArera <johnouma999@gmail.com>
2024-02-13 21:16:09 +0300

    feat: prevent default and reset form

--- src/components/AddDestinationForm.jsx
+++ src/components/AddDestinationForm.jsx
@@ -123,6 +123,19 @@ const AddDestinationForm = () =>{
             })
             if (response.ok){
                 alert("Successful!");
+                setFormData({
+
+                    name: "",
+                    location: "",
+                    image: "",
+                    description: "",
+                    reviews:[],
+                    services: {
+                        images:[],
+                        Accomodation: "",
+                        otherServices: []
+                    }
+                })
             }else{
                 throw new Error("Failed to post data!");
             }
@@ -133,7 +146,8 @@ const AddDestinationForm = () =>{
         }
     }
     // Checks if all input areas are filled before submitting data
-    const handleSubmit = () =>{
+    const handleSubmit = event =>{
+        event.preventDefault();
         if (formData.name.length > 0 &&
             formData.location.length > 0 &&
             formData.image.length > 0 &&
@@ -147,107 +161,106 @@ const AddDestinationForm = () =>{
     }
 
     return (
-        <form id="add-destination" onSubmit={handleSubmit} action="submit">
-            <label className="label" htmlFor="label">Facility Name</label>
-            <br />
-            <input 
-                className="add-destination-form"
-                type="text" 
-                name="name" 
-                value={formData.name} 
-                onChange={handleChange} 
-                placeholder="Enter facility name" 
-            />
-            <br />
-            <label className="label" htmlFor="label">Facility Location</label>
-            <br />
-            <input 
-                className="add-destination-form"
-                type="text" 
-                name="location" 
-                value={formData.location} 
-                onChange={handleChange} 
-                placeholder="Enter facility's location" 
-            />
-            <br />
-            <label className="label" htmlFor="label">Facility Image</label>
-            <br />
-            <input 
-                className="add-destination-form"
-                type="text" 
-                name="image" 
-                value={formData.image} 
-                onChange={handleChange} 
-                placeholder="Enter facility's image" 
-            />
-            <br />
-            <label className="label" htmlFor="label">Facility Image</label>
-            <br />
-            <textarea  
-                style={{
-                    resize: "vertical",
-                    minHeight: "100px"
-                }}
-                id="text-area" 
-                placeholder="Enter your facility description"
-                name="description"
-                value={formData.description}
-                onChange={handleChange}
-            >
-
-            </textarea>
-            <br />
-            <select 
-                name="Accomodation" 
-                id="Accomodation" 
-                onChange={handleSelectChange}
-                value={formData.services.Accomodation}
-            >
-                <option value="All">Select Accomodation type</option>
-                <option value="Available">Available</option>
-                <option value="Unavailable">Unavailable</option>
-            </select>
-            <br />
-            {formData.services.images.map((image, index) => (
-                <div key={index}>
-                    <label className="label" htmlFor={`image-${index}`}>Facility Image {index + 1}</label>
-                    <br />
-                    <input
-                        className="add-destination-form"
-                        type="text"
-                        id={`image-${index}`}
-                        name={`image-${index}`}
-                        value={image}
-                        onChange={e => handleImageChange(index, e.target.value)}
-                        placeholder="Enter facility's image"
-                    />
-                </div>
-            ))}
-            <br />
-            {/* Button to dynamically add more images */}
-            <button type="button" onClick={handleAddImage}>Add Image</button>
-            <br />
-            {formData.services.otherServices.map((service, index) => (
-                <div key={index}>
-                    <label className="label" htmlFor={`otherServices-${index}`}>Facility Sevices {index + 1}</label>
-                    <br />
-                    <input
-                        className="add-destination-form"
-                        type="text"
-                        id={`otherServices-${index}`}
-                        name={`otherServices-${index}`}
-                        value={service}
-                        onChange={e => handleServiceChange(index, e.target.value)}
-                        placeholder="Enter facility's other services"
-                    />
-                </div>
-            ))}
-            {/* Button to dynamically add more images */}
-            <button type="button" onClick={handleAddService}>Add Service</button>
-            <br />
-            {error && <p style={{ color: "red" }}>{error}</p>}
-            <button type="submit">Submit</button>
-        </form>
+        <div id="add-destination-card">
+            <form id="add-destination" onSubmit={handleSubmit} action="submit">
+                <label className="label" htmlFor="label">Facility Name</label>
+                <br />
+                <input 
+                    className="add-destination-form"
+                    type="text" 
+                    name="name" 
+                    value={formData.name} 
+                    onChange={handleChange} 
+                    placeholder="Enter facility name" 
+                />
+                <br />
+                <label className="label" htmlFor="label">Facility Location</label>
+                <br />
+                <input 
+                    className="add-destination-form"
+                    type="text" 
+                    name="location" 
+                    value={formData.location} 
+                    onChange={handleChange} 
+                    placeholder="Enter facility's location" 
+                />
+                <br />
+                <label className="label" htmlFor="label">Facility Image</label>
+                <br />
+                <input 
+                    className="add-destination-form"
+                    type="text" 
+                    name="image" 
+                    value={formData.image} 
+                    onChange={handleChange} 
+                    placeholder="Enter facility's image" 
+                />
+                <br />
+                <label className="label" htmlFor="label">Facility Description</label>
+                <br />
+                <textarea  
+                    style={{
+                        resize: "vertical",
+                        minHeight: "100px"
+                    }}
+                    id="text-area" 
+                    placeholder="Enter your facility description"
+                    name="description"
+                    value={formData.description}
+                    onChange={handleChange}
+                >
+                </textarea>
+                <br />
+                <select 
+                    name="Accomodation" 
+                    id="Accomodation" 
+                    onChange={handleSelectChange}
+                    value={formData.services.Accomodation}
+                >
+                    <option value="All">Select Accomodation type</option>
+                    <option value="Available">Available</option>
+                    <option value="Unavailable">Unavailable</option>
+                </select>
+                <br />
+                {formData.services.images.map((image, index) => (
+                    <div key={index}>
+                        <label className="label" htmlFor={`image-${index}`}>Facility Image {index + 1}</label>
+                        <br />
+                        <input
+                            className="add-destination-form"
+                            type="text"
+                            id={`image-${index}`}
+                            name={`image-${index}`}
+                            value={image}
+                            onChange={e => handleImageChange(index, e.target.value)}
+                            placeholder="Enter facility's image"
+                        />
+                    </div>
+                ))}
+                <br />
+                <button type="button" onClick={handleAddImage}>Add Image</button>
+                <br />
+                {formData.services.otherServices.map((service, index) => (
+                    <div key={index}>
+                        <label className="label" htmlFor={`otherServices-${index}`}>Facility Sevices {index + 1}</label>
+                        <br />
+                        <input
+                            className="add-destination-form"
+                            type="text"
+                            id={`otherServices-${index}`}
+                            name={`otherServices-${index}`}
+                            value={service}
+                            onChange={e => handleServiceChange(index, e.target.value)}
+                            placeholder="Enter facility's other services"
+                        />
+                    </div>
+                ))}
+                <button type="button" onClick={handleAddService}>Add Service</button>
+                <br />
+                {error && <p style={{ color: "red" }}>{error}</p>}
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     )
 
 }
