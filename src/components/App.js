fcfa06677f6f01a2bd048ce35ed33d2159384905
Naomi Lagat <naomiejerutoe@gmail.com>
2024-02-13 21:16:04 +0300

    incorporate React Router for navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-
-import './App.css';
+import About from "./About";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <div>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
