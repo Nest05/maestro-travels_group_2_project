f7e2e303c64c7fb3e7308d771cc46a8952f52e14
Nestor Masinde <nestormasinde@gmail.com>
2024-02-13 16:28:43 +0300

    feat: Add font and icons

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import {  DiAptana } from "react-icons/di";
 
 function App() {
   return (
     <div className="App">
-      
+     <button><DiAptana size={30} /></button>
+     <h1>We here</h1>
     </div>
   );
 }
