0dab57d7ddd704df2b5e622f5259b8e415c79a90
Naomi Lagat <naomiejerutoe@gmail.com>
2024-02-14 15:52:16 +0300

    unchanged

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
-
 import About from "./About";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Navbar from "./Navbar";
-import {  DiAptana } from "react-icons/di";
+import { DiAptana } from "react-icons/di";
 import { FiMenu } from "react-icons/fi";
 
-const y = 10; 
-
+const y = 10;
 
 function App() {
   return (
