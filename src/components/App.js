534357fb846817b0e1d74555f1d19af44b8bd251
Naomi Lagat <naomiejerutoe@gmail.com>
2024-02-14 16:19:11 +0300

    format code

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,6 @@ import Details from "./Details";
 import Home from "./Home";
 import { Switch, Route } from "react-router-dom";
 import Navbar from "./Navbar";
-// import {  DiAptana } from "react-icons/di";
-// import { FiMenu } from "react-icons/fi";
 import AddDestinationForm from "./AddDestinationForm";
 import Login from "./Login";
 import { useState, useEffect } from "react";
