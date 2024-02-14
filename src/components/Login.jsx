a1bc1dc1ae6d441abb50dc6b004fb1c452bc2a00
oliverfadhil <fadhilioliver1@gmail.com>
2024-02-14 15:28:43 +0300

    code update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,76 +1,80 @@
-import React from "react";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import AddDestinationForm from "./AddDestinationForm";
+const url = "http://localhost:3000/users"
 
-const Login = (props) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  const navigate = useNavigate();
+const Login = () =>{
 
-  const onButtonClick = () => {
-    setEmailError("");
-    setPasswordError("");
+    const [users, setUsers] = useState([]);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [loginError, setLoginError] = useState([]);
+    const [userData, setUserData] = useState({
+        username: "",
+        password: ""
+    })
 
-    if ("" === email) {
-      setEmailError("Please enter your email");
-      return;
+    const handleChange = event =>{
+        const {name, value} = event.target;
+        setUserData(prevData => ({
+            ...prevData,
+            [name]: value,
+        }))
     }
 
-    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-      setEmailError("Please enter a valid email");
-      return;
+    useEffect(() =>{
+        const fetchUsers = async () =>{
+            try{
+                const response = await fetch(url);
+                const data = await response.json();
+                setUsers(data)
+
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fetchUsers()
+    }, [])
+
+    const confirmPassword = () =>{
+        const userExists = users.some(user => user.username === userData.username && user.password === userData.password);
+        if (userExists){
+            setIsLoggedIn(true)
+        }else{
+            setLoginError("Incorrect username or password")
+        }
     }
 
-    if ("" === password) {
-      setPasswordError("Please enter a password");
-      return;
-    }
+    return (
+        <div>
+            {!isLoggedIn ?
 
-    if (password.length < 7) {
-      setPasswordError("The password must be 8 characters or longer");
-      return;
-    }
-  };
-};
-return (
-  <div className={"mainContainer"}>
-    <div className={"titleContainer"}>
-      <div>Login</div>
-    </div>
-    <br />
-    <div className={"inputContainer"}>
-      <input
-        value={email}
-        placeholder="Enter your email here"
-        onChange={(ev) => setEmail(ev.target.value)}
-        className={"inputBox"}
-      />
-      <label className="errorLabel">{emailError}</label>
-    </div>
+                (<div>
+                <label htmlFor="login"></label>
+                <br />
+                <input 
+                    name="username"
+                    type="text" 
+                    placeholder="Enter username"
+                    value={userData.username} 
+                    onChange={handleChange}
+                />
+                <br />
+                <input 
+                    name="password"
+                    type="password" 
+                    placeholder="Enter username" 
+                    value={userData.password}
+                    onChange={handleChange} 
+                />
+                <br />
+                {loginError && <p style={{color: "red"}}>{loginError}</p>}
+                <button onClick={confirmPassword}>Continue</button>
+            </div>
+            ):(
+                <AddDestinationForm tourGuide={userData.username} />
+            )
+            }
+        </div>
+    )
+}
 
-    <br />
-    <div className={"inputContainer"}>
-      <input
-        value={password}
-        placeholder="Enter your password here"
-        onChange={(ev) => setPassword(ev.target.value)}
-        className={"inputBox"}
-      />
-      <label className="errorLabel">{passwordError}</label>
-    </div>
-
-    <br />
-    <div className={"inputContainer"}>
-      <input
-        className={"inputButton"}
-        type="button"
-        onClick={onButtonClick}
-        value={"Log in"}
-      />
-    </div>
-  </div>
-);
-
-export default Login;
+export default Login
