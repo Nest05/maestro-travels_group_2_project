967b4e840201c336fad9501d4d37660c3c4e6220
Nestor Masinde <nestormasinde@gmail.com>
2024-02-15 09:15:46 +0300

    feat: Add data and classes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./navbar.css"
 
 function Navbar() {
   return (
-    <div>
+    <div className="navBar">
       
-          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/"} className="navBar1">Home</NavLink>
         
           <NavLink to={"/about"}>About</NavLink>
 
