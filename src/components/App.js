8ea817443ed923beddbcf6956c037c853c5cde9f
OumaArera <johnouma999@gmail.com>
2024-02-16 11:55:43 +0300

    fix: pass filteredData to Home as prop

--- src/components/App.js
+++ src/components/App.js
@@ -31,6 +31,10 @@ function App() {
     };
     fetchData();
   }, []);
+
+   useEffect(() => {
+    setSearchTerm(""); // Reset searchTerm on component mount or page reload
+  }, []);
   
    const filteredData = jsonData.filter(item => {
     return (
@@ -58,7 +62,8 @@ function App() {
       </header>
       <Switch>
         <Route exact path="/">
-          <Home jsonData={jsonData} />
+          <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          <Home jsonData={filteredData} />
         </Route>
         <Route exact path="/about">
           <About />
